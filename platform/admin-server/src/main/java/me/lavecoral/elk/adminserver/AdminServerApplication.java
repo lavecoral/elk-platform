b497aee4991797242862a2a19d449ac6ddd6be57
Lave <despair1989@126.com>
2021-03-28 00:50:54 +0800

    add Spring Boot Admin

---
+++ platform/admin-server/src/main/java/me/lavecoral/elk/adminserver/AdminServerApplication.java
@@ -0,0 +1,20 @@
+package me.lavecoral.elk.adminserver;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author lave
+ * @date 2021-03-28 00:33:00
+ */
+@EnableAdminServer
+@EnableDiscoveryClient
+@SpringBootApplication
+public class AdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServerApplication.class, args);
+    }
+}
---
+++ platform/admin-server/src/test/java/me/lavecoral/elk/adminserver/AdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.lavecoral.elk.adminserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
