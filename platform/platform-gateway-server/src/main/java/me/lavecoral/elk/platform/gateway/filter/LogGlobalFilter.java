3abc6f6c2277f8524d0b2f41c762744b75a40cc4
Lave <despair1989@126.com>
2021-04-23 22:12:00 +0800

    网关添加全局过滤器

---
+++ platform/platform-gateway-server/src/main/java/me/lavecoral/elk/platform/gateway/filter/LogGlobalFilter.java
@@ -0,0 +1,49 @@
+package me.lavecoral.elk.platform.gateway.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author lave
+ * @date 2021/4/23 22:06
+ */
+@Component
+public class LogGlobalFilter implements GlobalFilter, Ordered {
+    private static final Logger LOG = LoggerFactory.getLogger(LogGlobalFilter.class);
+    /**
+     * Process the Web request and (optionally) delegate to the next {@code WebFilter}
+     * through the given {@link GatewayFilterChain}.
+     *
+     * @param exchange the current server exchange
+     * @param chain    provides a way to delegate to the next filter
+     * @return {@code Mono<Void>} to indicate when request processing is complete
+     */
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        LOG.info("request : {} path: {}", exchange.getRequest().getId(), exchange.getRequest().getPath());
+        return chain.filter(exchange);
+    }
+
+    /**
+     * Get the order value of this object.
+     * <p>Higher values are interpreted as lower priority. As a consequence,
+     * the object with the lowest value has the highest priority (somewhat
+     * analogous to Servlet {@code load-on-startup} values).
+     * <p>Same order values will result in arbitrary sort positions for the
+     * affected objects.
+     *
+     * @return the order value
+     * @see #HIGHEST_PRECEDENCE
+     * @see #LOWEST_PRECEDENCE
+     */
+    @Override
+    public int getOrder() {
+        return -1;
+    }
+}
